1bcddddb847c89bd2c2c0bd87705fbb839473804
JINSHANSHAN\LiTian <1406957473@qq.com>
2017-10-27 17:35:50 +0800

    模版初始化提交

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,28 +5,24 @@
 
       <el-form-item prop="username">
         <span class="svg-container svg-container_login">
-          <huang-svg name="icon-zhanghao" :size='100' :color="'red'" />
+          <huang-svg name="icon-zhanghao" :size='20' :color="'#fff'" />
         </span>
         <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="邮箱" />
       </el-form-item>
 
       <el-form-item prop="password">
         <span class="svg-container">
-          <huang-svg name="icon-406biaoqing_mogui" />
+          <huang-svg name="icon-lock" :size='20' :color="'#fff'" />
         </span>
         <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
           placeholder="密码" />
-        <span class='show-pwd' @click='showPwd'><huang-svg name="eye" /></span>
+        <span class='show-pwd' @click='showPwd'>
+          <huang-svg name="icon-biyan" :size='20'  :color="'#fff'"  />
+        </span>
       </el-form-item>
 
       <el-button type="primary" style="width:100%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">登录</el-button>
     </el-form>
-
-    <el-dialog title="第三方验证" :visible.sync="showDialog">
-      本地不能模拟，请结合自己业务进行模拟！！！<br/><br/><br/>
-      邮箱登录成功,请选择第三方验证<br/>
-      <social-sign />
-    </el-dialog>
 
   </div>
 </template>
@@ -59,8 +55,7 @@ export default {
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
       pwdType: 'password',
-      loading: false,
-      showDialog: false
+      loading: false
     }
   },
   methods: {
@@ -78,7 +73,6 @@ export default {
           this.$store.dispatch('LoginByUsername', this.loginForm).then(() => {
             this.loading = false
             this.$router.push({ path: '/' })
-                // this.showDialog = true
           }).catch(() => {
             this.loading = false
           })
@@ -96,6 +90,7 @@ export default {
   @bg:#2d3a4b;
   @dark_gray:#889aa4;
   @light_gray:#eee;
+
   .login-container {
     height: 100vh;
     background-color: @bg;
